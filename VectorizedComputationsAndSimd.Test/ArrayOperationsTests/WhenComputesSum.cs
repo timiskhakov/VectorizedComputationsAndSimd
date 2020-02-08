f03afa335689e61f375b50c728b1d6257671412f
Tim Iskhakov <timiskhakov@gmail.com>
2020-02-08 16:15:36 +0200

    Refactor tests to remove duplication of expected values

--- VectorizedComputationsAndSimd.Test/ArrayOperationsTests/WhenComputesSum.cs
+++ VectorizedComputationsAndSimd.Test/ArrayOperationsTests/WhenComputesSum.cs
@@ -6,21 +6,21 @@ namespace VectorizedComputationsAndSimd.Test.ArrayOperationsTests
     [TestClass]
     public class WhenComputesSum
     {
-        private readonly List<int[]> _arrays = new List<int[]>
+        private readonly List<(int[], int)> _arrays = new List<(int[], int)>
         {
-            new [] {63, -43, 123, 1567, 123, 565, 4307, -128, 0},
-            new [] {98, 123, -9, -135, -1245, 75, 157, -68, -135, 545},
-            new [] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25}
+            (new [] {63, -43, 123, 1567, 123, 565, 4307, -128, 0}, 6577),
+            (new [] {98, 123, -9, -135, -1245, 75, 157, -68, -135, 545}, -594),
+            (new [] {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25}, 325)
         };
         
         [DataTestMethod]
-        [DataRow(0, 6577)]
-        [DataRow(1, -594)]
-        [DataRow(2, 325)]
-        public void UsingLinqSum_ShouldReturnSum(int index, int expected)
+        [DataRow(0)]
+        [DataRow(1)]
+        [DataRow(2)]
+        public void UsingLinqSum_ShouldReturnSum(int index)
         {
             // Arrange
-            var matrix = _arrays[index];
+            var (matrix, expected) = _arrays[index];
             
             // Act
             var sum = ArrayOperations.LinqSum(matrix);
@@ -30,13 +30,13 @@ namespace VectorizedComputationsAndSimd.Test.ArrayOperationsTests
         }
         
         [DataTestMethod]
-        [DataRow(0, 6577)]
-        [DataRow(1, -594)]
-        [DataRow(2, 325)]
-        public void UsingNaiveSum_ShouldReturnSum(int index, int expected)
+        [DataRow(0)]
+        [DataRow(1)]
+        [DataRow(2)]
+        public void UsingNaiveSum_ShouldReturnSum(int index)
         {
             // Arrange
-            var matrix = _arrays[index];
+            var (matrix, expected) = _arrays[index];
             
             // Act
             var sum = ArrayOperations.NaiveSum(matrix);
@@ -46,13 +46,13 @@ namespace VectorizedComputationsAndSimd.Test.ArrayOperationsTests
         }
         
         [DataTestMethod]
-        [DataRow(0, 6577)]
-        [DataRow(1, -594)]
-        [DataRow(2, 325)]
-        public void UsingSimdSum_ShouldReturnSum(int index, int expected)
+        [DataRow(0)]
+        [DataRow(1)]
+        [DataRow(2)]
+        public void UsingSimdSum_ShouldReturnSum(int index)
         {
             // Arrange
-            var matrix = _arrays[index];
+            var (matrix, expected) = _arrays[index];
             
             // Act
             var sum = ArrayOperations.SimdSum(matrix);
